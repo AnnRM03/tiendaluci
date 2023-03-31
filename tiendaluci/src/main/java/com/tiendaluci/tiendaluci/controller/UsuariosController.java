a3bc2942377b8fbdec974f8dd0ad235949ca37b0
regristroalrm <registroalrm@gmail.com>
2023-03-30 21:00:23 -0600

    usuario

--- tiendaluci/src/main/java/com/tiendaluci/tiendaluci/controller/UsuariosController.java
+++ tiendaluci/src/main/java/com/tiendaluci/tiendaluci/controller/UsuariosController.java
@@ -20,7 +20,7 @@ public class UsuariosController {
 	public String mostrarIndex(Model model) {
 		model.addAttribute("usuarios", serviceUsuarios.obtnerUsuarios());
 		System.out.println(serviceUsuarios.obtnerUsuarios());
-		return "usuarios/listaUsuarios";
+		return "usuario/listaUsuarios";
 	}
 
 }
