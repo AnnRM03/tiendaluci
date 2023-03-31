f3298016f784d9f28b4a611c97a0800a090f6b48
regristroalrm <registroalrm@gmail.com>
2023-03-31 11:38:35 -0600

    commit

---
+++ tiendaluci/src/main/java/com/tiendaluci/tiendaluci/entity/Usuario.java
@@ -0,0 +1,10 @@
+package com.tiendaluci.tiendaluci.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "Usuarios")
+public class Usuario {
+	
+}
