99d9568703c14db66d555300187d49978a7e210c
regristroalrm <registroalrm@gmail.com>
2023-04-06 20:21:03 -0500

    commit

--- tiendaluci/src/main/java/com/tiendaluci/tiendaluci/repository/PerfilesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiendaluci.tiendaluci.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tiendaluci.tiendaluci.entity.Perfil;
-
-public interface PerfilesRepository extends JpaRepository<Perfil, Integer> {
-
-}
