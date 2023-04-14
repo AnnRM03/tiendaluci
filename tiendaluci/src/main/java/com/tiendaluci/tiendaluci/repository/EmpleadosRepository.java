762fb4a79f1d541fc5ddd4be2b58a443b6dc1ef8
regristroalrm <registroalrm@gmail.com>
2023-04-14 15:48:53 -0500

    commit

--- tiendaluci/src/main/java/com/tiendaluci/tiendaluci/repository/EmpleadosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiendaluci.tiendaluci.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tiendaluci.tiendaluci.entity.Empleado;
-
-public interface EmpleadosRepository extends JpaRepository<Empleado, Integer> {
-
-}
--- tiendaluci/src/main/java/com/tiendaluci/tiendaluci/repository/ProductosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tiendaluci.tiendaluci.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tiendaluci.tiendaluci.entity.Producto;
-
-public interface ProductosRepository extends JpaRepository<Producto, Integer> {
-
-}
